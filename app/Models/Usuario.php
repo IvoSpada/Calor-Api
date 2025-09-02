a47e3ba070b72d7f04621c7c66b650d4bc3e9e5b
ivo <ivospada03@gmail.com>
2025-09-02 16:18:30 -0300

    MFS Usuario

---
+++ app/Models/Usuario.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Usuario extends Model
+{
+    use HasFactory;
+
+    protected $table = 'usuario'; // porque no es plural
+    public $timestamps = false; // 👈 evita el error
+
+
+    protected $fillable = [
+        'nombre',
+        'email',
+        'peso',
+        'altura',
+        'edad',
+        'objetivo',
+    ];
+
+    // Un usuario puede tener muchas dietas
+    public function dietas()
+    {
+        return $this->hasMany(Dieta::class, 'usuario_id');
+    }
+
+    // Un usuario puede tener muchas comidas registradas
+    public function comidasUsuario()
+    {
+        return $this->hasMany(ComidaUsuario::class, 'usuario_id');
+    }
+}
